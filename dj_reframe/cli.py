da15c17f1672fb52d83cfa4571ae5650fe99c94b
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-06-10 15:03:04 -0300

    chore: organize imports on Ruff

--- dj_reframe/cli.py
+++ dj_reframe/cli.py
@@ -1,15 +1,15 @@
 import argparse
-import django
 import os
 import platform
 import subprocess
 import sys
-from dj_reframe import __version__
-from django.conf import settings
-from django.core.management import call_command
 from pathlib import Path
 
+import django
+from django.conf import settings
+from django.core.management import call_command
 
+from dj_reframe import __version__
 
 """
  Sets the folder in the user's "Home" directory to store custom templates.
